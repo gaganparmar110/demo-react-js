785076b29668965b6479a3ed06f66fdc0cc2e6ef
Gagan Parmar <gaganparmar110@gmail.com>
2022-04-03 12:53:44 +0530

    Configure basic structure

---
+++ task-reactjs/src/Components/Admin/Admins.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// STYLES
+import { Wrapper } from "./Admins.style";
+
+function Admins() {
+    return (
+        <Wrapper>
+                <h1 className='title-wrapper'>Welcome to admin list!!</h1>
+        </Wrapper>
+    );
+};
+
+export default Admins;
---
+++ task-reactjs/src/Components/Admin/Admins.style.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    .title-wrapper {
+        justify-content : space-between;
+    }
+`
